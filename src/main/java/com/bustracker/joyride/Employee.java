8f48b0ae594529884b17505a4651093378270bf2
IraPalabay0321 <iramishael.palabay@lorma.edu>
2026-02-02 16:38:01 +0800

    new employees table

---
+++ src/main/java/com/bustracker/joyride/Employee.java
@@ -0,0 +1,15 @@
+package com.bustracker.joyride;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+    @Table(name = "employees")
+    public class Employee {
+        @Id
+        @GeneratedValue(strategy =GenerationType.IDENTITY)
+        long id;
+}
